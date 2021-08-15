4274aa7db3aeadb5fd5998c00c2161a8aa137177
sibimahesh <sibi.mahesh240894@gmail.com>
2021-08-15 13:53:26 +0530

    Enable offline support with service worker

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+let cacheData = 'porfolioV2';
+
+this.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                './css/style.css',
+                './img/profile-pic.jpg',
+                './img/sprite.svg',
+                './img/Sibi_Mahesh_Resume.docx',
+                './favicon.ico',
+                'https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Nunito:300,300i',
+                './index.html',
+                '/'
+            ]);
+        })
+    )
+})
+
+this.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
+    )
+})
