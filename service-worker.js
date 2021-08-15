075329c68961a097b390d278b3f446ce26afc503
sibimahesh <sibi.mahesh240894@gmail.com>
2021-08-15 20:50:56 +0530

    attempt 14

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-let cacheData = 'portfolioV5';
+let cacheData = 'portfolioV6';
 console.log("Inside service worker");
 this.addEventListener("install", event => {
     event.waitUntil(
         caches.open(cacheData).then((cache) => {
             cache.addAll([
-                'https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Nunito:300,300i',
+                'https://sibimahesh.github.io/Portfolio/css/style.css',
                 '/'
             ]);
         })
