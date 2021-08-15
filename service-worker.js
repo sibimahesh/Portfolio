34d763a07e005aceb03f55f1b507f8373e52650a
sibimahesh <sibi.mahesh240894@gmail.com>
2021-08-15 21:09:23 +0530

    attempt 22

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-let cacheData = 'portfolioV10';
+let cacheData = 'portfolioV11';
 console.log("Inside service worker");
 this.addEventListener("install", event => {
     event.waitUntil(
         caches.open(cacheData).then((cache) => {
             cache.addAll([
-                './css/style.css',
-                './img/profile-pic.jpg',
-                './img/sprite.svg',
-                './img/Sibi_Mahesh_Resume.docx',
-                './favicon.ico',
+                '.css/style.css',
+                '.img/profile-pic.jpg',
+                '.img/sprite.svg',
+                '.img/Sibi_Mahesh_Resume.docx',
+                '.favicon.ico',
                 'https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Nunito:300,300i',
                 './'
             ]);
